1eb91927d9dbe48c37d51a52d3f3ff947721366a
Matthew9King <116584151+Matthew9King@users.noreply.github.com>
2022-11-29 13:50:52 -0500

    Update Proj.lean

--- Proj.lean
+++ Proj.lean
@@ -27,7 +27,7 @@ structure group (G : Type) where
   is_ident : ∀g : G, mul e g = g ∧ mul g e = g 
   Ex_inv : ∀g : G, ∃g_inv, mul (g_inv) g = e ∧ mul g (g_inv) = e
 
-structure permutation (α : Type) where 
+@[ext] structure permutation (α : Type) where 
   to_fun : α → α 
   IsInv : ∃ g, g ∘ to_fun = id ∧ to_fun ∘ g = id 
 
@@ -66,8 +66,6 @@ def eIden : (permutation α) where
     exact rfl 
     exact rfl
 
-
--- STILL NEED HELP WITH GETTING THE MONOID THING MADE
 theorem eIdent {a: permutation α} : comp eIden a = a ∧ comp a eIden = a := by
   apply And.intro
   have l1 : (comp eIden a).to_fun = a.to_fun := by
@@ -77,51 +75,22 @@ theorem eIdent {a: permutation α} : comp eIden a = a ∧ comp a eIden = a := by
     rw[that]
     rfl
 
-  have ⟨a', l4⟩ := a.IsInv
   have ⟨c', l5⟩ := (comp eIden a).IsInv
   rw[l1] at l5 
-  have that : c' = a' := by 
-    have sla : c' ∘(a.to_fun∘id) = id := l5.left
-    rw[←l4.left] at sla
-    have sla2 : c'∘ (a.to_fun ∘ a') ∘ a.to_fun = (a'∘a.to_fun) := sla
-    rw[l4.left] at sla2 
-    rw[l4.right] at sla2
-    have h : id∘ a.to_fun = a.to_fun := rfl
-    rw[h] at sla2
-    have slb : (c'∘ a.to_fun) ∘ a' = id ∘ a' := by 
-      rw[sla2]
-    have slb2 : c' ∘ (a.to_fun ∘ a') = id ∘ a' := slb
-    rw[l4.right] at slb2
-    exact slb2
-  have l1b : (comp eIden a).IsInv = a.IsInv := rfl
-  
-
-  have l2 : (comp a eIden).to_fun = a.to_fun := by
-    have sl1 : (comp a eIden).to_fun = a.to_fun ∘ eIden.to_fun := rfl 
-    rw[sl1]
-    have that : eIden.to_fun = @id α := by rfl 
-    rw[that]
-    rfl
-  have ⟨a', l4⟩ := a.IsInv
-  have ⟨c', l5⟩ := (comp a eIden).IsInv
-  rw[l2] at l5
-  have sl1 : a' = c' := by
-    have sl1a : c' ∘ (a.to_fun ∘ a') = id ∘ a' := by
-      rw[←l5.left]
-      rfl 
-    rw[l4.right] at sl1a 
-    have ob : c'∘ id = c' := rfl 
-    have ob2 : id ∘ a' = a' := rfl 
-    rw[←ob, ←ob2]
-    exact Eq.symm sl1a
-  have invpart : (comp a eIden).IsInv = a.IsInv := rfl
-
-
+  ext n 
+  have partext : permutation.to_fun (comp eIden a) = permutation.to_fun a := l1 
+  rw[partext]
+  rfl 
 
 --NEED HELP TO UN-SORRY THIS
 theorem exists_inv {g : permutation α} : ∃j, comp j g = eIden ∧ comp g j = eIden := by
+  apply Exists.intro 
+  apply And.intro 
+  ext n
   have ⟨g', l1⟩ := g.IsInv 
-
+  sorry
+  sorry 
+  
   
 theorem associat {a b c : permutation α} : comp (comp a b) c = comp a (comp b c) := rfl
 
